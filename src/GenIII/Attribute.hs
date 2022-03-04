31290f175752a62c4236c2272ad694846f8a8d20
Tommi Salenius <tommisalenius@gmail.com>
2022-03-05 00:08:09 +0200

    Fixed possibleAbility function's signature

--- src/GenIII/Attribute.hs
+++ src/GenIII/Attribute.hs
@@ -5,8 +5,11 @@ module GenIII.Attribute (
   
 import qualified GenII.Attribute as Prev
 import GenII.Attribute hiding (PokemonSYM())
+import GenIII.Ability
 
 class Prev.PokemonSYM p => PokemonSYM p where
-  possibleAbility :: p Ability -> p AbilityOp
+  possibleAbility :: p GenderOp -> p Ability -> p AbilityOp
+
+infixl 5 `possibleAbility`
   
 data AbilityOp = AbilityOp
